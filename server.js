37dbda94c97a1f49854c0d825a1d3b01ac68247f
Nicholas Zuber <zuber.nicholas@gmail.com>
2017-08-08 22:25:57 -0700

    Update server file

--- server.js
+++ server.js
@@ -3,23 +3,45 @@
 // ES5 here so we don't have to transpile this
 const config = require('./config')
 const routes = require('./routes')
-// const db = require('./db')
 const path = require('path')
 const fs = require('fs')
 const http = require('http')
+const cors = require('cors')
 const express = require('express')
+const subdomain = require('express-subdomain')
+const bodyParser = require('body-parser')
+const RateLimit = require('express-rate-limit')
 
+// Create server
 const app = express()
 const server = http.createServer(app)
 
+// Connect to db
+const db = require('./db')
+
+// Configure views
 app.set('view engine', 'jade')
 app.set('views', './views')
 
+// Body parser for post requests and CORS
+app.use(bodyParser.json())
+app.use(cors())
+
+// Rate limiting
+app.enable('trust proxy')
+
+// Hook up API subdomain
+const api_router = express.Router()
+require('./routes/api')(api_router)
+app.use(subdomain('api', api_router))
+app.use('/api', api_router)
+
+// Static files
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Add general app router
 routes(app)
 
-// THIS IS OUT OF DATE WITH THE DEV SERVER
-
+// Start server
 server.listen(config.app.server.port)
 console.log(`http server listening on port ${config.app.server.port}...`)
